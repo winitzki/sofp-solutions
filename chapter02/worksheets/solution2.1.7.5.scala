a82d3efd8757cfe4e749b45c0b5c1e97b05e2ba4
Andreas Roehler <andreas.roehler@online.de>
2023-11-27 19:15:45 +0100

    solution2.1.7.5.scala assert added
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

--- chapter02/worksheets/solution2.1.7.5.scala
+++ chapter02/worksheets/solution2.1.7.5.scala
@@ -9,10 +9,8 @@ contains the strings from p ordered according to the corresponding numbers from 
 val p = Seq("a", "b", "c")
 val q = Seq(10, -1, 5)
 
-p.zip(q).sortBy(_._2).map{case (x, y) => x}
+val result = p.zip(q).sortBy(_._2).map{case (x, y) => x}
 
-// scala> :load solution2.1.7.5.scala
-// Loading solution2.1.7.5.scala...
-// p: Seq[String] = List(a, b, c)
-// q: Seq[Int] = List(10, -1, 5)
-// res0: Seq[String] = List(b, c, a)
+// test
+val expected = List("b", "c", "a")
+assert(result == expected)
