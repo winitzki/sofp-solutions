bec4f1abc37662e4732e2603f388eb635619b75c
Andreas Roehler <andreas.roehler@online.de>
2023-11-27 09:06:17 +0100

    solution2.1.7.3_AR.scala fixed
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

--- chapter02/worksheets/solution2.1.7.3_AR.scala
+++ chapter02/worksheets/solution2.1.7.3_AR.scala
@@ -5,14 +5,8 @@ Given two sequences p: Seq[String] and q: Seq[Boolean] of equal length, compute 
 Seq[String] with those elements of p for which the corresponding element of q is true.
 Hint: use zip, map, filter. */ 
 
-// val names: List[String]        = List("Joe", "Bob", "Mary")
-// val a: List[Boolean]           = List(true, false, true)
-// val b: List[(String, Boolean)] = names.zip(a)
-// val c: List[Any]               = b.map { case (x, y) => if (y == true) x }
-// val d: List[Any]               = c.filter(_ != (()))
-
-List("Joe", "Bob", "Mary").zip(List(true, false, true)).map { case (x, y) => if (y == true) x }.filter(_ != (()))
+Seq("Joe", "Bob", "Mary").zip(Seq(true, false, true)).map { case (x, y) => if (y == true) x }.filter(_ != (())).map{ case x => x.toString }
 
 // scala> :load solution2.1.7.3_AR.scala
 // Loading solution2.1.7.3_AR.scala...
-// res17: List[Any] = List(Joe, Mary)
+// res17: Seq[String] = List(Joe, Mary)
