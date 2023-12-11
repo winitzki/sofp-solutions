3d55bd63147cd93c1cfe858c6b6f947657d1d125
Andreas Roehler <andreas.roehler@online.de>
2023-12-11 12:15:06 +0100

    solution2.1.7.9_as_function fixed
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

--- chapter02/worksheets/solution2.1.7.9_as_function.scala
+++ chapter02/worksheets/solution2.1.7.9_as_function.scala
@@ -14,6 +14,7 @@ Hint: use groupBy, map, sum.
 def totalCount(arg: Seq[(String, Int)]) = {
   // val a: Seq[(String, Int)] =  Seq(("pear", 2), ("apple", 2), ("apple", 6), ("pear", 1), ("lemon", 4), ("apple", 2), ("apple", 2))
   arg.groupBy(s => s._1).map { case (x, y) => (x, y.map(_._2).sum) }
+}
 
 // scala> totalCount(Seq(("pear", 2), ("apple", 2), ("apple", 6), ("pear", 1), ("lemon", 4), ("apple", 2), ("apple", 2)))
 // res37: scala.collection.immutable.Map[String,Int] = Map(pear -> 3, apple -> 12, lemon -> 4)
