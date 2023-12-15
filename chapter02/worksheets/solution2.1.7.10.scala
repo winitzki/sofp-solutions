39903b0da169a4623d070b1009a03c89caab3f85
Andreas Roehler <andreas.roehler@online.de>
2023-12-15 19:08:07 +0100

    Exercise 2.1.7.10 minor fix
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

--- chapter02/worksheets/solution2.1.7.10.scala
+++ chapter02/worksheets/solution2.1.7.10.scala
@@ -2,7 +2,7 @@
 
 /** 
   Exercise 2.1.7.10
-  Given a Seq[List[Int]], compute a new Seq[List[Int]] where each list contains three largest elements from the initial list (or fewer than three if the initial inner list is
+  Given a Seq[List[Int]], compute a new Seq[List[Int]] where each list contains three largest elements from the initial list (or fewer than three if the initial list is
   shorter). So, for the input:
 
   Seq(List(0, 50, 5, 10, 30), List(100), List(1, 2, 200, 20))
