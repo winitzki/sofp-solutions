57b9507cdd5d08108e3fe538ff4561ef92de73a3
Andreas Roehler <andreas.roehler@online.de>
2023-12-26 17:32:39 +0100

    missing object, fixed
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

--- chapter01/worksheets/solution1.1.1.1_as_function_AR.scala
+++ chapter01/worksheets/solution1.1.1.1_as_function_AR.scala
@@ -5,11 +5,13 @@
   Find the product of integers from 1 to 10 (the factorial of 10).
   */
 
-def myFactorial(a: Int): Int = {
-  (1 to a).foldRight(1)(_ * _)  
+object MyFactorial {
+  def myFactorial(a: Int): Int = {
+    (1 to a).foldRight(1)(_ * _)
+  }
 }
 
-val result =  myFactorial(10)
+val result =  MyFactorial.myFactorial(10)
 val expected = 3628800
 
 println("result: %s".format(result))
