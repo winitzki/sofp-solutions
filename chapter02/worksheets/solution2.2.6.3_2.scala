448f76b9cffdc7ffc0a575503bbdefc09b2984fe
Andreas Roehler <andreas.roehler@online.de>
2024-09-07 13:20:07 +0200

    #63, avoid calling ‘reverse’
    Signed-off-by: Andreas Roehler <andreas.roehler@online.de>

--- chapter02/worksheets/solution2.2.6.3_2.scala
+++ chapter02/worksheets/solution2.2.6.3_2.scala
@@ -13,7 +13,7 @@
 def zipWithIndex[A](xs: Seq[A]): Seq[(A, Int)] = {
   type Acc = Seq[(A, Int)]
   def init: Acc = Seq((xs.head, -1))
-  xs.foldLeft(init) { (x, y) => (y, x(0)._2 + 1) +: x }.reverse.tail
+  xs.foldLeft(init) { (x, y) => x :+ (y, x.last._2 + 1) }.tail
 }
 
 val expected: Seq[(String, Int)] = List(("a", 0), ("b", 1), ("c", 2), ("d", 3))
